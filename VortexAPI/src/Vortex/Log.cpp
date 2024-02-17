75f4a6100d40866c43a1f5c2190cd829ea8c3cbc
Adrian <49879773+adriankwan108@users.noreply.github.com>
2024-02-17 18:13:56 +0800

    adjust naming of spdlog loggers

--- VortexAPI/src/Vortex/Log.cpp
+++ VortexAPI/src/Vortex/Log.cpp
@@ -8,10 +8,10 @@ namespace VX
     void Log::Init()
     {
         spdlog::set_pattern("%^[%T] %n: %v%$");
-        s_CoreLogger = spdlog::stdout_color_mt("Vortex");
+        s_CoreLogger = spdlog::stdout_color_mt("VortexAPI");
         s_CoreLogger->set_level(spdlog::level::trace);
         
-        s_ClientLogger = spdlog::stdout_color_mt("App");
+        s_ClientLogger = spdlog::stdout_color_mt("Engine");
         s_ClientLogger->set_level(spdlog::level::trace);
 
         VX_CORE_INFO("Log initiated...");
