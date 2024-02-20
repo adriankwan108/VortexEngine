7489a749ff7f1490fd8db45344df2d3dc5d68537
Adrian <49879773+adriankwan108@users.noreply.github.com>
2024-02-21 02:37:26 +0800

    Implement Layer & LayerStack

---
+++ VortexAPI/src/Vortex/Layer.cpp
@@ -0,0 +1,14 @@
+#include "VortexPCH.hpp"
+#include "Layer.hpp"
+
+namespace VX
+{
+    Layer::Layer(const std::string& name): m_DebugName(name)
+    {
+    }
+
+    Layer::Layer()
+    {
+
+    }
+}
