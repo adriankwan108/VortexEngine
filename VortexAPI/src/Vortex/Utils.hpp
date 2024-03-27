fa14d7942a6ff3271d15939786cfe6c7d9a7e639
Adrian <adriankwan108@gmail.com>
2024-03-27 12:25:52 +0800

    Update utils readfile with absolute path implicitly

--- VortexAPI/src/Vortex/Utils.hpp
+++ VortexAPI/src/Vortex/Utils.hpp
@@ -7,8 +7,49 @@ namespace VX
 {
     namespace Utils
     {
+        static std::string GetProjectRootPath()
+        {
+            std::filesystem::path absolutePath = std::filesystem::current_path();
+
+            std::filesystem::path tempPath = absolutePath.root_path();
+
+            for (const auto& path : absolutePath)
+            {
+                if (!path.has_stem())
+                {
+                    continue;
+                }
+
+                tempPath += path;
+    #if defined(__APPLE__)
+                tempPath += "/";
+    #elif defined(_MSC_VER)
+                tempPath += "\\";
+    #endif
+
+                if (path.stem() == "VortexEngine")
+                {
+                    break;
+                }
+            }
+
+            return tempPath.string();
+        }
+        static std::string AbsolutePath(const std::string& inputPath)
+        {
+            //std::string path = std::move(inputPath);
+            std::string path = inputPath;
+    #if defined(__APPLE__)
+            return GetProjectRootPath() + path;
+    #elif defined(_MSC_VER)
+            std::replace(path.begin(), path.end(), '/', '\\');
+            return GetProjectRootPath() + "\\" + path;
+    #endif
+        }
+
         static std::vector<char> readFile(const std::string& filename) {
-            std::ifstream file(filename, std::ios::ate | std::ios::binary);
+            
+            std::ifstream file(AbsolutePath(filename), std::ios::ate | std::ios::binary);
             std::vector<char> buffer(0);
             if (!file.good())
             {
@@ -32,52 +73,11 @@ namespace VX
             return buffer;
         }
 
-        static std::string GetProjectRootPath()
-        {
-            std::filesystem::path absolutePath = std::filesystem::current_path();
-
-            std::filesystem::path tempPath = absolutePath.root_path();
-
-            for (const auto& path : absolutePath)
-            {
-                if (!path.has_stem())
-                {
-                    continue;
-                }
-
-                tempPath += path;
-#if defined(__APPLE__)
-                tempPath += "/";
-#elif defined(_MSC_VER)
-                tempPath += "\\";
-#endif
-
-                if (path.stem() == "VortexEngine")
-                {
-                    break;
-                }
-            }
-
-            return tempPath.string();
-        }
-
-        static std::string AbsolutePath(const std::string& inputPath)
-        {
-            //std::string path = std::move(inputPath);
-            std::string path = inputPath;
-#if defined(__APPLE__)
-            return GetProjectRootPath() + path;
-#elif defined(_MSC_VER)
-            std::replace(path.begin(), path.end(), '/', '\\');
-            return GetProjectRootPath() + "\\" + path;
-#endif
-        }
-
-        template <typename T>
-        size_t CalculateMemorySize(const std::vector<T>& v)
-        {
-            return sizeof(v[0]) * v.size();
-        }
+    template <typename T>
+    size_t CalculateMemorySize(const std::vector<T>& v)
+    {
+        return sizeof(v[0]) * v.size();
+    }
 #define MEM_SIZE(...)    ::VX::Utils::CalculateMemorySize(__VA_ARGS__)
     }
 }
